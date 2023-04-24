1e1e2fcdc0d80d5f4ada99a021cc76219c893d02
emerickgilbert <1950301@cegepgranby.qc.ca>
2023-04-23 20:38:38 -0400

    test de kacke

--- Denis3/script.js
+++ Denis3/script.js
@@ -1,22 +1,23 @@
 var params;
 
-var TILE_WIDTH
-var TILE_HEIGHT
+var TILE_WIDTH;
+var TILE_HEIGHT;
 var NUM_COLS = 10;
 var NUM_ROWS = 10;
-var WIDTH
-var HEIGHT
+var WIDTH;
+var HEIGHT;
 
 const canvas = document.getElementById("canvas");
 const canvas_container = document.getElementById("canvas-container");
-
+const overviewCanvas = document.getElementById("overview");
+const overviewCtx = overviewCanvas.getContext("2d");
 // dragging and zooming
 var isDragging = false;
 var lastX, lastY;
 var scale = 1;
 const zoom_max = 5;
-var positionX = 0
-var positionY = 0
+var positionX = 0;
+var positionY = 0;
 
 // Keep track of which tiles are currently loaded
 var loadedTiles = {};
@@ -24,147 +25,249 @@ var loadedTiles = {};
 const TILE_URL_TEMPLATE = "http://127.0.0.1:8081/tile_{x}_{y}.jpg";
 
 async function init() {
-    params = await (await fetch("http://127.0.0.1:8081/params.json")).json();
+  params = await (await fetch("http://127.0.0.1:8081/params.json")).json();
 
-    TILE_WIDTH = params.width
-    TILE_HEIGHT = params.height
-    TILE_MOD_WIDTH = params.modWidth
-    TILE_MOD_HEIGHT = params.modHeight
-    NUM_COLS = params.x
-    NUM_ROWS = params.y
-    WIDTH = (NUM_COLS - 1) * TILE_WIDTH + (TILE_MOD_WIDTH == 0? TILE_WIDTH: TILE_MOD_WIDTH)
-    HEIGHT = (NUM_ROWS - 1) * TILE_HEIGHT + (TILE_MOD_HEIGHT== 0? TILE_HEIGHT: TILE_MOD_HEIGHT)
-    
-    canvas.height = canvas.clientHeight
-    canvas.width = canvas.clientWidth
-    canvas.addEventListener("mousedown", function(event) {
-        isDragging = true;
-        lastX = event.clientX;
-        lastY = event.clientY;
-        event.preventDefault();
-    });
-    canvas.addEventListener("mousemove", function(event) {
-        if (isDragging) {
-            var deltaX = (event.clientX - lastX) / scale;
-            var deltaY = (event.clientY - lastY) / scale;
-            lastX = event.clientX;
-            lastY = event.clientY;
-            //prevent outbound
-            // left
-            if(positionX - deltaX < 0){
-                deltaX = positionX
-            }
-            // right
-            else if(positionX - deltaX > WIDTH - canvas.clientWidth /scale){
-                deltaX = positionX - WIDTH + canvas.clientWidth / scale
-            }
-            positionX -= deltaX
-            // top
-            if(positionY - deltaY < 0){
-                deltaY = positionY
-            }
-            // bottom
-            else if(positionY - deltaY > HEIGHT- canvas.clientHeight / scale){
-                deltaY = positionY - HEIGHT + canvas.clientHeight / scale
-            }
-            
-            positionY -= deltaY
-            canvas.getContext("2d").translate(deltaX, deltaY);
-            renderTiles();
-            event.preventDefault();
-        }
-    });
-    canvas.addEventListener("mouseup", function(event) {
-        isDragging = false;
-        event.preventDefault();
-    });
-    canvas.addEventListener("wheel", function(event) {
-        var delta
-        if (scale < 0.8) {
-            delta = event.deltaY / 10000
-          } else if (scale < 2.5) {
-            delta = event.deltaY / 5000
-          } else {
-            delta = event.deltaY / 1000
-          }
-        var oldScale = scale;
-        scale *= (1 + delta);
-        scale = Math.max(zoom_min(), Math.min(zoom_max, scale));
-        //var offsetX = canvas.width / 2;
-        //var offsetY = canvas.height / 2;
-        canvas.getContext("2d").translate(positionX, positionY);
-        canvas.getContext("2d").scale(scale / oldScale, scale / oldScale);
+  TILE_WIDTH = params.width;
+  TILE_HEIGHT = params.height;
+  TILE_MOD_WIDTH = params.modWidth;
+  TILE_MOD_HEIGHT = params.modHeight;
+  NUM_COLS = params.x;
+  NUM_ROWS = params.y;
+  WIDTH =
+    (NUM_COLS - 1) * TILE_WIDTH +
+    (TILE_MOD_WIDTH == 0 ? TILE_WIDTH : TILE_MOD_WIDTH);
+  HEIGHT =
+    (NUM_ROWS - 1) * TILE_HEIGHT +
+    (TILE_MOD_HEIGHT == 0 ? TILE_HEIGHT : TILE_MOD_HEIGHT);
 
-        //keep center
-        positionX += (canvas.width / oldScale - canvas.width / scale) / 2
-        positionY += (canvas.height / oldScale - canvas.height / scale) / 2
+  canvas.height = canvas.clientHeight;
+  canvas.width = canvas.clientWidth;
+  canvas.addEventListener("mousedown", function (event) {
+    isDragging = true;
+    lastX = event.clientX;
+    lastY = event.clientY;
+    event.preventDefault();
+  });
+  canvas.addEventListener("mousemove", function (event) {
+    if (isDragging) {
+      var deltaX = (event.clientX - lastX) / scale;
+      var deltaY = (event.clientY - lastY) / scale;
+      lastX = event.clientX;
+      lastY = event.clientY;
+      //prevent outbound
+      // left
+      if (positionX - deltaX < 0) {
+        deltaX = positionX;
+      }
+      // right
+      else if (positionX - deltaX > WIDTH - canvas.clientWidth / scale) {
+        deltaX = positionX - WIDTH + canvas.clientWidth / scale;
+      }
+      positionX -= deltaX;
+      // top
+      if (positionY - deltaY < 0) {
+        deltaY = positionY;
+      }
+      // bottom
+      else if (positionY - deltaY > HEIGHT - canvas.clientHeight / scale) {
+        deltaY = positionY - HEIGHT + canvas.clientHeight / scale;
+      }
 
-        //prevent outbound
-        positionX = Math.max(0,Math.min(positionX, WIDTH - canvas.clientWidth / scale))
-        positionY = Math.max(0,Math.min(positionY, HEIGHT- canvas.clientHeight / scale))
+      positionY -= deltaY;
+      canvas.getContext("2d").translate(deltaX, deltaY);
+      renderTiles();
+      event.preventDefault();
+    }
+  });
+  canvas.addEventListener("mouseup", function (event) {
+    isDragging = false;
+    event.preventDefault();
+  });
+  canvas.addEventListener("wheel", function (event) {
+    var delta;
+    if (scale < 0.8) {
+      delta = -event.deltaY / 10000;
+    } else if (scale < 2.5) {
+      delta = -event.deltaY / 5000;
+    } else {
+      delta = -event.deltaY / 1000;
+    }
+    var oldScale = scale;
+    scale *= 1 + delta;
+    scale = Math.max(zoom_min(), Math.min(zoom_max, scale));
+    //var offsetX = canvas.width / 2;
+    //var offsetY = canvas.height / 2;
+    canvas.getContext("2d").translate(positionX, positionY);
+    canvas.getContext("2d").scale(scale / oldScale, scale / oldScale);
 
-        canvas.getContext("2d").translate(-positionX, -positionY);
-        renderTiles();
-        event.preventDefault();
-    });
+    //keep center
+    positionX += (canvas.width / oldScale - canvas.width / scale) / 2;
+    positionY += (canvas.height / oldScale - canvas.height / scale) / 2;
 
-    window.addEventListener("resize", function() {
-        canvas.height = canvas.clientHeight
-        canvas.width = canvas.clientWidth
-        
-        positionX = Math.max(0,Math.min(positionX, WIDTH - canvas.clientWidth / scale))
-        positionY = Math.max(0,Math.min(positionY, HEIGHT- canvas.clientHeight / scale))
-        canvas.getContext("2d").translate(-positionX, -positionY);
-        renderTiles()
-    })
+    //prevent outbound
+    positionX = Math.max(
+      0,
+      Math.min(positionX, WIDTH - canvas.clientWidth / scale)
+    );
+    positionY = Math.max(
+      0,
+      Math.min(positionY, HEIGHT - canvas.clientHeight / scale)
+    );
 
-    renderTiles()
+    canvas.getContext("2d").translate(-positionX, -positionY);
+    renderTiles();
+    event.preventDefault();
+  });
+
+  window.addEventListener("resize", function () {
+    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
+
+    positionX = Math.max(
+      0,
+      Math.min(positionX, WIDTH - canvas.clientWidth / scale)
+    );
+    positionY = Math.max(
+      0,
+      Math.min(positionY, HEIGHT - canvas.clientHeight / scale)
+    );
+    canvas.getContext("2d").translate(-positionX, -positionY);
+    renderTiles();
+  });
+
+  renderTiles();
+}
+
+// Add a function to render the overview map and viewport rectangle
+function renderOverview() {
+  overviewCanvas.width = WIDTH / 10;
+  overviewCanvas.height = HEIGHT / 10;
+  overviewCtx.clearRect(0, 0, overviewCanvas.width, overviewCanvas.height);
+
+  for (let col = 0; col < NUM_COLS; col++) {
+    for (let row = 0; row < NUM_ROWS; row++) {
+      let tileX = (col * TILE_WIDTH) / 10;
+      let tileY = (row * TILE_HEIGHT) / 10;
+      let tileWidth =
+        col === NUM_COLS - 1 && TILE_MOD_WIDTH !== 0
+          ? TILE_MOD_WIDTH / 10
+          : TILE_WIDTH / 10;
+      let tileHeight =
+        row === NUM_ROWS - 1 && TILE_MOD_HEIGHT !== 0
+          ? TILE_MOD_HEIGHT / 10
+          : TILE_HEIGHT / 10;
+      let tileName = getTileName(col, row);
+      let tileURL = getTileURL(col, row);
+
+      if (!loadedTiles[tileName]) {
+        // Load the tile if it hasn't been loaded yet
+        var img = new Image();
+        img.src = tileURL;
+        img.onload = function () {
+          loadedTiles[tileName] = img;
+          overviewCtx.drawImage(img, tileX, tileY, tileWidth, tileHeight);
+        };
+      } else {
+        overviewCtx.drawImage(
+          loadedTiles[tileName],
+          tileX,
+          tileY,
+          tileWidth,
+          tileHeight
+        );
+      }
+    }
+  }
+
+  // Draw the viewport rectangle
+  overviewCtx.beginPath();
+  overviewCtx.rect(
+    positionX / 10,
+    positionY / 10,
+    canvas.clientWidth / (scale * 10),
+    canvas.clientHeight / (scale * 10)
+  );
+  overviewCtx.lineWidth = 2;
+  overviewCtx.strokeStyle = "red";
+  overviewCtx.stroke();
 }
 
 // Render the tiles that are currently visible on the canvas
 function renderTiles() {
-    var ctx = canvas.getContext("2d");
-    //ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    for (var col = 0; col < NUM_COLS; col++) {
-        for (var row = 0; row < NUM_ROWS; row++) {
-            var tileX = col * TILE_WIDTH;
-            var tileY = row * TILE_HEIGHT;
-            var tileWidth = (col == NUM_COLS - 1)?(TILE_MOD_WIDTH == 0? TILE_WIDTH: TILE_MOD_WIDTH):TILE_WIDTH
-            var tileHeight = (row == NUM_ROWS - 1)?(TILE_MOD_HEIGHT == 0? TILE_WIDTH: TILE_MOD_HEIGHT):TILE_HEIGHT
-            if (isTileVisible(tileX, tileY, tileWidth, tileHeight)) {
-                var tileName = getTileName(col, row);
-                if (!loadedTiles[tileName]) {
-                    var img = new Image();
-                    img.src = getTileURL(col, row);
-                    img.onload = function() {
-                        ctx.drawImage(img, tileX, tileY, tileWidth, tileHeight);
-                    };
-                    loadedTiles[tileName] = img;
-                } else {
-                    ctx.drawImage(loadedTiles[tileName], tileX, tileY, tileWidth, tileHeight);
-                }
-            }
-        }
+  var ctx = canvas.getContext("2d");
+  // Calculate the visible range of tiles
+  var startCol = Math.floor(positionX / TILE_WIDTH);
+  var startRow = Math.floor(positionY / TILE_HEIGHT);
+  var endCol = Math.min(
+    NUM_COLS - 1,
+    Math.ceil((positionX + canvas.clientWidth / scale) / TILE_WIDTH)
+  );
+  var endRow = Math.min(
+    NUM_ROWS - 1,
+    Math.ceil((positionY + canvas.clientHeight / scale) / TILE_HEIGHT)
+  );
+
+  // Load and draw the visible tiles
+  for (var col = startCol; col <= endCol; col++) {
+    for (var row = startRow; row <= endRow; row++) {
+      var tileX = col * TILE_WIDTH;
+      var tileY = row * TILE_HEIGHT;
+      var tileWidth =
+        col == NUM_COLS - 1
+          ? TILE_MOD_WIDTH == 0
+            ? TILE_WIDTH
+            : TILE_MOD_WIDTH
+          : TILE_WIDTH;
+      var tileHeight =
+        row == NUM_ROWS - 1
+          ? TILE_MOD_HEIGHT == 0
+            ? TILE_WIDTH
+            : TILE_MOD_HEIGHT
+          : TILE_HEIGHT;
+      var tileName = getTileName(col, row);
+      if (!loadedTiles[tileName]) {
+        // Load the image asynchronously and draw it once it's loaded
+        var img = new Image();
+        img.src = getTileURL(col, row);
+        img.onload = function () {
+          ctx.drawImage(img, tileX, tileY, tileWidth, tileHeight);
+        };
+        loadedTiles[tileName] = img;
+      } else {
+        // Draw the image if it's already loaded
+        ctx.drawImage(
+          loadedTiles[tileName],
+          tileX,
+          tileY,
+          tileWidth,
+          tileHeight
+        );
+      }
     }
+  }
+
+  renderOverview();
 }
 
 // Determine if a tile is currently visible on the canvas
 function isTileVisible(tileX, tileY, tileWidth, tileHeight) {
-    return (tileX + tileWidth)> positionX &&
-        tileX < (positionX + canvas.clientWidth / scale)&&
-        tileY + tileHeight > positionY &&
-        tileY < positionY + canvas.clientHeight / scale;
+  return (
+    tileX + tileWidth > positionX &&
+    tileX < positionX + canvas.clientWidth / scale &&
+    tileY + tileHeight > positionY &&
+    tileY < positionY + canvas.clientHeight / scale
+  );
 }
 
 // Get the filename or URL of a tile
 function getTileName(col, row) {
-    return "tile_" + col + "_" + row;
+  return "tile_" + col + "_" + row;
 }
 function getTileURL(col, row) {
-    return TILE_URL_TEMPLATE.replace("{x}", col + 1).replace("{y}", row + 1);
+  return TILE_URL_TEMPLATE.replace("{x}", col + 1).replace("{y}", row + 1);
 }
-function zoom_min(){
-    return Math.max(canvas.height / HEIGHT, canvas.width / WIDTH)
+function zoom_min() {
+  return Math.max(canvas.height / HEIGHT, canvas.width / WIDTH);
 }
 
 init();
--- script4.js
+++ script4.js
@@ -1,32 +1,95 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import TileImage from "ol/source/TileImage";
+import { createXYZ } from "ol/tilegrid";
+import Projection from "ol/proj/Projection";
+import { OverviewMap, defaults as defaultControls } from "ol/control";
 
-// Create an ImageBitmap from an image or canvas element
-const image = document.getElementById("myImage");
+const tileSize = [1000, 700];
+const extent = [0, 0, tileSize[0] * 20, tileSize[1] * 8];
 
-let imgUrl = "http://127.0.0.1:8081/tile_1_1.jpg";
+const projection = new Projection({
+  code: "custom",
+  units: "pixels",
+  extent: extent,
+});
 
-fetch(imgUrl)
-  .then((response) => response.blob())
-  .then((blob) => createImageBitmap(blob))
-  .then((imageBitmap) => {
-    canvas.width = imageBitmap.width;
-    canvas.height = imageBitmap.height;
+const resolutions = [1];
 
-    // Define the grid size and the size of each tile
-    const gridSize = 4;
-    const tileSize = canvas.width / gridSize;
+const tileGrid = createXYZ({
+  extent: extent,
+  tileSize: tileSize,
+  maxZoom: 20,
+});
 
-    // Loop through each row and column of the grid
-    for (let row = 0; row < gridSize; row++) {
-      for (let col = 0; col < gridSize; col++) {
-        // Draw the tile at the current row and column
-        const x = col * tileSize;
-        const y = row * tileSize;
-        ctx.drawImage(imageBitmap, x, y, tileSize, tileSize);
+const tileLayer = new TileLayer({
+  source: new TileImage({
+    projection: projection,
+    tileGrid: tileGrid,
+    tileUrlFunction: function (tileCoord) {
+      const [z, x, y] = tileCoord;
+
+      if (x >= 0 && x < 20 && y >= 0 && y < 8) {
+        return `http://127.0.0.1:8081/tile_${x + 1}_${y + 1}.jpg`;
+      } else {
+        return null;
       }
-    }
-  })
-  .catch((error) => console.error(error));
+    },
+  }),
+});
 
-// Set the canvas width and height to match the image size
+const overviewTileSize = [100, 70];
+const overviewExtent = [0, 0, tileSize[0] * 20, tileSize[1] * 8];
+
+// Define a new tile grid for the overview map
+const overviewTileGrid = createXYZ({
+  extent: overviewExtent,
+  tileSize: overviewTileSize,
+  maxZoom: 6, // Define a smaller number of zoom levels for the overview map
+});
+
+// Define a new tile layer for the overview map that uses the new tile grid
+const overviewTileLayer = new TileLayer({
+  source: new TileImage({
+    projection: projection,
+    tileGrid: overviewTileGrid,
+    tileUrlFunction: function (tileCoord) {
+      const [z, x, y] = tileCoord;
+
+      if (x >= 0 && x < 20 && y >= 0 && y < 8) {
+        return `http://127.0.0.1:8081/tile_${x + 1}_${y + 1}.jpg`;
+      } else {
+        return null;
+      }
+    },
+  }),
+});
+
+// Create the OverviewMap control using the new tile layer and tile grid
+const overviewMapControl = new OverviewMap({
+  layers: [overviewTileLayer],
+  view: new View({
+    projection: projection,
+    resolutions: overviewTileGrid.getResolutions(),
+    extent: overviewExtent,
+  }),
+});
+
+const map = new Map({
+  controls: defaultControls().extend([overviewMapControl]),
+  layers: [tileLayer],
+  target: "map",
+  view: new View({
+    center: [tileSize[0] * 10, tileSize[1] * 4],
+    zoom: 0,
+    minZoom: 0,
+    maxZoom: 20, // Increase maxZoom to allow more zoom levels
+    projection: projection,
+    extent: extent,
+    resolutions: resolutions,
+    constrainResolution: false, // Allow fractional zoom levels
+  }),
+});
+console.log("Main map extent:", extent);
+console.log("Overview map extent:", overviewExtent);
